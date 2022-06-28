d40fa7504bf9f5168ad9235087e30cc7b55d7106
KennyShi-designer <809420184@qq.com>
2022-06-28 23:38:19 +0800

    deploy

---
+++ precache-manifest.0e1318c6cb3ea7e9fbccd5fa0fc51797.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5f0d18009daa0b4014",
+    "url": "/moneyVue-website/css/app.898e7021.css"
+  },
+  {
+    "revision": "a3cb0fed8abbe254efad55550bba628c",
+    "url": "/moneyVue-website/fonts/iconfont.a3cb0fed.ttf"
+  },
+  {
+    "revision": "2aaf2d44d7b48e1939738e7f296bd76d",
+    "url": "/moneyVue-website/index.html"
+  },
+  {
+    "revision": "5a5f0d18009daa0b4014",
+    "url": "/moneyVue-website/js/app.1a7824ce.js"
+  },
+  {
+    "revision": "49040149c2e9f12e6d88",
+    "url": "/moneyVue-website/js/chunk-vendors.bda99394.js"
+  },
+  {
+    "revision": "d0bfaf7d4a8c57509eddad6a7edd9e57",
+    "url": "/moneyVue-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moneyVue-website/robots.txt"
+  }
+]);
